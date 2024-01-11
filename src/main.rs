255e62028987fdcbdcfded3b4bd277af0239b097
pryority <matthewapryor@gmail.com>
2024-01-11 15:44:44 -0500

    🎨 General update: dapp, dapp_tile, dapp_grid, etc.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() -> Result<(), slint::PlatformError> {
 
     ui.on_request_open_dapp(move || {
         let ui = ui_handle.unwrap();
-        println!("{}", ui.get_dapp());
-        ui.set_dapp(ui.get_dapp());
+        // println!("{}", ui.get_active_dapp());
+        ui.set_active_dapp(ui.get_active_dapp());
     });
 
     // ui.on_request_active_chain(move || {
@@ -17,8 +17,26 @@ fn main() -> Result<(), slint::PlatformError> {
     // });
 
     ui.on_request_send_tokens(move || {
-        println!("Sent 0.25 ETH to vitalik.eth!");
+        println!("Initiating SEND sequence.");
     });
+
+    // ui.on_request_dapp_config(move |d| {
+    //     let ui = ui_handle.unwrap();
+
+    //     // match the dapp passed in as 'd' in the closure to list of dapps
+    //     let selected_dapp = DappData {
+    //         name: d.name,
+    //         logo: d.logo,
+    //         url: d.url,
+    //     };
+    //     // println!("Getting dapp config: {:#?}", selected_dapp);
+    //     let dapp_result = format!(
+    //         "Name: {} | Logo: {} | URL: {}",
+    //         selected_dapp.name, selected_dapp.logo, selected_dapp.url
+    //     );
+    //     println!("{}", dapp_result);
+    //     ui.set_dapp(selected_dapp);
+    // });
 
     ui.on_request_all_balances(move || {
         println!("100 ETH, 30,000 OP, 4.2 BTC");
