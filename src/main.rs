97716ab201d9dda36b428a396d159bf13a3bff80
pryority <matthewapryor@gmail.com>
2024-01-12 12:59:34 -0500

    🎨 Helios client can return data when app is open

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,55 @@
 slint::include_modules!();
+use dotenv::dotenv;
+use std::{path::PathBuf};
 use helios::{
-    // checkpoints::CheckpointFallback,
-    config::{checkpoints, networks},
+    client::{ClientBuilder, Client, FileDB},
+    config::{checkpoints, networks, networks::Network},
 };
+use tracing::info;
+use eyre::Report;
 
 #[tokio::main]
-async fn main() -> Result<(), slint::PlatformError> {
+async fn main() -> Result<(), Report> {
+    dotenv().ok();
+    let untrusted_rpc_url = std::env::var("EXECUTION")
+    .expect("Please set the 'EXECUTION' environment variable with the RPC URL");
+    info!("Using untrusted RPC URL [REDACTED]");
+
+    let consensus_rpc = "https://www.lightclientdata.org";
+    info!("Using consensus RPC URL: {}", consensus_rpc);
+
+    let mut client: Client<FileDB> = match ClientBuilder::new()
+    .network(Network::MAINNET)
+    .consensus_rpc(consensus_rpc)
+    .execution_rpc(&untrusted_rpc_url)
+    .load_external_fallback()
+    .data_dir(PathBuf::from("/tmp/helios"))
+    .build() {
+        Ok(client) => client,
+        Err(err) => {
+            // Handle the error here, log it, print a message, etc.
+            // For now, let's print the error to the console.
+            eprintln!("Error building Helios client: {}", err);
+            return Err(err);
+        }
+    };
+
+    println!(
+        "Built client on network \"{}\" with external checkpoint fallbacks",
+        Network::MAINNET
+    );
+
+    client.start().await.unwrap();
+    client.wait_synced().await;
+    
+    let head_block_num = client.get_block_number().await.unwrap();
+    println!("synced up to block: {}", head_block_num);
+
     // Create an instance of the generated component
     let ui = AppWindow::new()?;
 
     // Clone strong handles for properties
+    let client_handle = ui.as_weak();
     let active_dapp_handle = ui.as_weak();
     let active_chain_handle = ui.as_weak();
     let checkpoint_handle = ui.as_weak();
@@ -25,6 +65,11 @@ async fn main() -> Result<(), slint::PlatformError> {
         .fetch_latest_checkpoint(&networks::Network::MAINNET)
         .await
         .unwrap();
+
+    // ui.on_sync(move || {
+    //     let ui = client_handle.unwrap();
+    //     ui.set_sync_status(synced());
+    // });
 
     ui.on_fetch_latest_mainnet_checkpoint(move || {
         let ui = checkpoint_handle.unwrap();
@@ -60,5 +105,5 @@ async fn main() -> Result<(), slint::PlatformError> {
         println!("100 ETH, 30,000 OP, 4.2 BTC");
     });
 
-    ui.run()
+    Ok(ui.run()?)
 }
