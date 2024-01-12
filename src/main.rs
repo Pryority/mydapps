68706c3060b348bd6f4a84ccdd3a7869e742bce4
pryority <matthewapryor@gmail.com>
2024-01-11 19:52:22 -0500

    🐛 Active Dapp now available

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,34 @@
 slint::include_modules!();
+// slint::slint!(import { DappTile } from "../ui/dapps/dapp_tile.slint";);
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
     let ui_handle = ui.as_weak();
 
-    ui.on_request_open_dapp(move || {
+    // let dapp_tile = DappTile::new()?;
+    // let dapp_tile_handle = dapp_tile.as_weak();
+
+    // DAPPS ----------------------------------
+
+    // ui.on_request_open_dapp(move || {
+    //     let ui = ui_handle.unwrap();
+    //     // println!("{}", ui.get_active_dapp());
+    //     ui.set_active_dapp(ui.get_active_dapp());
+    // });
+
+    ui.on_select_dapp(move |d| {
         let ui = ui_handle.unwrap();
-        // println!("{}", ui.get_active_dapp());
-        ui.set_active_dapp(ui.get_active_dapp());
+        ui.set_active_dapp(d.clone());
+        let dapp = ui.get_active_dapp();
+        // println!("main.rs Passed-In dapp: {:?}", d);
+        println!("Active Dapp: {:?}", dapp.name);
     });
 
-    // ui.on_request_active_chain(move || {
-    //     let ui = ui_handle.unwrap();
-    //     println!("{}", ui.get_active_chain());
-    //     ui.get_active_chain();
-    // });
+    // PERSONAL -------------------------------
 
     ui.on_request_send_tokens(move || {
         println!("Initiating SEND sequence.");
     });
-
-    // ui.on_request_dapp_config(move |d| {
-    //     let ui = ui_handle.unwrap();
-
-    //     // match the dapp passed in as 'd' in the closure to list of dapps
-    //     let selected_dapp = DappData {
-    //         name: d.name,
-    //         logo: d.logo,
-    //         url: d.url,
-    //     };
-    //     // println!("Getting dapp config: {:#?}", selected_dapp);
-    //     let dapp_result = format!(
-    //         "Name: {} | Logo: {} | URL: {}",
-    //         selected_dapp.name, selected_dapp.logo, selected_dapp.url
-    //     );
-    //     println!("{}", dapp_result);
-    //     ui.set_dapp(selected_dapp);
-    // });
 
     ui.on_request_all_balances(move || {
         println!("100 ETH, 30,000 OP, 4.2 BTC");
